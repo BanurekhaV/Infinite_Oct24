f5ce68fd648e95b91c04b17455b1be75012d114d
BanurekhaV <rekhasairam@hotmail.com>
2024-10-25 17:04:35 +0530

    Day 1 Basics

---
+++ CSharp/Day1_basic/Day1_basic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Day1_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("welcome to Csharp");
+            Console.Read();
+        }
+    }
+}
