f12c7aa901d8a87f6a42c470be73a876e9c62be0
BanurekhaV <rekhasairam@hotmail.com>
2024-10-28 17:01:05 +0530

    day1 csharp

---
+++ CSharp/Day1_Dotnet/Day1_Dotnet/MethodsandParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day1_Dotnet
+{
+    class MethodsnParameters
+    {
+        public static void testfunc()
+        {
+            Console.WriteLine("static function of Methods class");
+        }
+    }
+
+    class Instance_Static
+    {
+        public static void Evennos(int num) //static function
+        {
+            if (num % 2 == 0) Console.WriteLine("Even number");
+        }
+
+        public int multiply(int x, int y)  // instance function
+        {
+            Console.WriteLine("hi");
+            return x * y;           
+        }
+        static void Main()  //static functions
+        {
+            Instance_Static instanceobj = new Instance_Static();
+            Instance_Static instanceobj2 = new Instance_Static();
+            Instance_Static.Evennos(5);
+            Evennos(6);  //calling static function of the same class
+            MethodsnParameters.testfunc(); //calling static function of a different class, we need 
+                                           // refer it with the classname
+            
+            Console.WriteLine(instanceobj.multiply(5, 6));
+            Console.Read();
+        }
+    }
+}
--- CSharp/Day1_Dotnet/Day1_Dotnet/Program.cs
+++ CSharp/Day1_Dotnet/Day1_Dotnet/Program.cs
@@ -5,14 +5,40 @@ namespace Day1_Dotnet
 {
     class Program
     {
-        static int add(int a, int b)
+         static int add(int a, int b)
         {
             return a + b;
         }
 
-        static void welcome(string msg)
+      static void welcome(string msg)
         {
             Console.WriteLine(msg);
+        }
+
+        public static void NullEg()
+        {
+            int ? TicketsonSale = null;
+            int AvailableTickets;
+
+            if(TicketsonSale==null)
+            {
+                AvailableTickets = 0;
+            }
+            else
+            {
+                AvailableTickets = (int)TicketsonSale;  //or
+
+                AvailableTickets = TicketsonSale.Value;
+            }
+            Console.WriteLine("Available Tickets = {0}", AvailableTickets);
+
+            //the above if and an else involving null can be replaced with null coalescing operator ??
+
+            Console.WriteLine("*************************");
+
+            int ? tktonsale = null; //or null
+            int avltkts = tktonsale ?? 0;
+            Console.WriteLine(avltkts);
         }
         static void Main(string[] args)
         {
@@ -38,33 +64,43 @@ namespace Day1_Dotnet
              i = Convert.ToInt32("9007"); //conversion function
              Console.WriteLine(i + " with conversion function"); */
 
-            Console.WriteLine("----------Parse and Try Parse Example----------");
+            //Console.WriteLine("----------Parse and Try Parse Example----------");
 
-            string str = "245abc";
-            //  int result = int.Parse(str); parse throws error when unable to parse
-            int result = 0;
-            bool answer = int.TryParse(str, out result);
-            Console.WriteLine(result + " answer is " + answer);
+            //string str = "245abc";
+            ////  int result = int.Parse(str); parse throws error when unable to parse
+            //int result = 0;
+            //bool answer = int.TryParse(str, out result);
+            //Console.WriteLine(result + " answer is " + answer);
 
-            Console.WriteLine("--------Boxing and UnBoxing--------");
+            //Console.WriteLine("--------Boxing and UnBoxing--------");
 
-            int n = 10;
-            object obj;
-            obj = n;  //boxing
-            Console.WriteLine(obj);
-            string s = "AAAA";
-            obj = s;  //implicit 
+            //int n = 10;
+            //object obj;
+            //obj = n;  //boxing
+            //Console.WriteLine(obj);
+            //string s = "AAAA";
+            //obj = s;  //implicit 
 
-            float salary;
-            Console.WriteLine("Enter salary , boolean and date of joining:");
-            salary = float.Parse(Console.ReadLine());  //unboxing
+            //float salary;
+            //Console.WriteLine("Enter salary , boolean and date of joining:");
+            //salary = float.Parse(Console.ReadLine());  //unboxing
             
-            bool b;
-            b = Convert.ToBoolean(Console.ReadLine());
+            //bool b;
+            
+            
+            //b = Convert.ToBoolean(Console.ReadLine());
            
-            DateTime doj;
-            doj = Convert.ToDateTime(Console.ReadLine());
-            Console.WriteLine($"Salary is {salary}, Boolean Value is {b} and Dateof Joining is {doj} ");
+            //DateTime doj;
+            //doj = Convert.ToDateTime(Console.ReadLine());
+            //Console.WriteLine($"Salary is {salary}, Boolean Value is {b} and Dateof Joining is {doj} ");
+
+            Console.WriteLine("----------Calling other Functions---------");
+            NullEg();  //calling a static function
+
+            //int res = add(6, 10);
+            // Console.WriteLine(res);
+          //  Console.WriteLine(add(6,10));
+           // welcome("hello");
             Console.Read();
         }
     } 
