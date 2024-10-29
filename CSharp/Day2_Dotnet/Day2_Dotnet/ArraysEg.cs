9c9b29bdeb8021a3302a69462115f7435034a365
BanurekhaV <rekhasairam@hotmail.com>
2024-10-29 13:30:15 +0530

    Day2 Csharp

---
+++ CSharp/Day2_Dotnet/Day2_Dotnet/ArraysEg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Dotnet
+{
+    class ArraysEg
+    {
+        public static void SingleDimension()
+        {
+            int[] data = new int[] { 42, 6, 8, 35, 3 };
+            Console.WriteLine(data.Length + " " + data.Rank);
+            Console.WriteLine("----------Before Sort---------");
+            foreach (int x in data)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("----------After Sort----------");
+            Array.Sort(data);
+            //Array.Reverse(data);
+            foreach (int x in data)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        //single 
+        
+    }
+
+    class TestArrays
+    {
+        public static void Main()
+        {
+            ArraysEg.SingleDimension();
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day2_Dotnet/Day2_Dotnet/Loops.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Dotnet
+{
+    class Loops
+    {
+        public void WhileLoop()
+        {
+            int i = 1;  //1. initializing a counter
+            while(i<5) // 2. checking a condition
+            {
+                Console.WriteLine(i);
+                i++;  // 3. change of initial value
+            }
+        }
+
+        public void DoWhileLoop()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 5);
+        }
+
+        public void ForLoop()
+        {
+            int total = 0;
+            for(int i=0; i<=5;i++)
+            {
+                if (i == 3)
+                    // break;
+                    continue;
+                total += i;
+                
+            }
+            Console.WriteLine(total);
+        }
+
+        static void Main()
+        {
+            Loops loops = new Loops();
+            Console.WriteLine("Do While Loop");
+            loops.DoWhileLoop();
+            Console.WriteLine("While Loop");
+            loops.WhileLoop();
+            Console.WriteLine("For Loop");
+            loops.ForLoop();
+            Console.Read();
+        }
+    }
+
+}
