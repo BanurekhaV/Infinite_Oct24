3a002b1dc7d8b10b1bd8c2798e04698bd5d57129
BanurekhaV <rekhasairam@hotmail.com>
2024-11-06 18:31:17 +0530

    Day6 csharp

---
+++ CSharp/Day6_Dotnet/Day6_Dotnet/ExceptionsEg1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Dotnet
+{
+    class ExceptionsEg1
+    {
+        static void Main()
+        {
+            int a, b, c;
+            try
+            {
+                Console.WriteLine("Enter 2 nos :");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+
+                int[] arr = { 1, 2, 3, 4 };
+                Console.WriteLine(arr[6]);
+                Console.WriteLine("C is " + c);
+            }
+            
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Source + " "+ fe.Message);
+                Console.WriteLine("Please enter only numbers");
+            }
+
+            catch(DivideByZeroException de)
+            {
+                Console.WriteLine(de.StackTrace + " " + de.Message);
+                Console.WriteLine("Cannot divide a number by 0");
+            }
+            //catch(IndexOutOfRangeException ie)
+            //{
+            //    Console.WriteLine("Cannot read beyond the Limit");
+            //}
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong try later..");
+            }
+
+            finally
+            {
+                Console.WriteLine("Reached Finally..");
+            }
+                       
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day6_Dotnet/Day6_Dotnet/UserDefinedException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Dotnet
+{
+    
+    class VotingException : ApplicationException
+    {
+        public VotingException(string msg) : base(msg) { }
+    }
+
+    class Vote
+    {
+        int age;
+
+        public void getAge()
+        {
+            Console.WriteLine("Enter your Age :");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if(age < 18)
+            {
+                throw (new VotingException("Age should be greater than 18"));
+            }
+            else
+                Console.WriteLine("Thanks for Voting ...");
+        }
+    }
+
+    class UserDefinedException
+    {
+        static void Main()
+        {
+            Vote vote = new Vote();
+            try
+            {
+                vote.getAge();
+            }
+
+            catch(VotingException ve)
+            {
+                Console.WriteLine(ve.Message);
+            }
+            Console.Read();
+
+        }
+    }
+}
