b06d98f6e8bd287072cf4df489508b3f7c83beed
BanurekhaV <rekhasairam@hotmail.com>
2024-11-07 18:26:27 +0530

    Day6 csharp

---
+++ CSharp/Day7_Dotnet/Day7_Dotnet/IEnumerable_IEnumeratorEg.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Dotnet
+{
+    class IEnumerable_IEnumeratorEg
+    {
+        static void Main()
+        {
+            //List Initialization
+            List<string> colors = new List<string>() { "Red", "Blue", "Green", "Yellow", "White" };
+
+            // IEnumerable<string> iecolors =(IEnumerable<string>) colors; // by typecasting
+
+            IEnumerable<string> iecolors = colors;
+
+            foreach(string c in iecolors)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("-----------");
+            //with IEnumerator
+            IEnumerator<string> ienc = colors.GetEnumerator();
+            while(ienc.MoveNext())
+            {
+                Console.WriteLine(ienc.Current);
+            }
+            Console.Read();
+        }
+
+    }
+}
