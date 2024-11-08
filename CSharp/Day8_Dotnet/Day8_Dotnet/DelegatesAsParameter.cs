8a5fb01ea6f237467ec157aaffd211eda4303242
BanurekhaV <rekhasairam@hotmail.com>
2024-11-08 17:05:31 +0530

    day7 csharp

---
+++ CSharp/Day8_Dotnet/Day8_Dotnet/DelegatesAsParameter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_Dotnet
+{   
+    class DelegatesAsParameter
+    {
+        delegate int Arithmetic(int x, int y);
+        static void Main()
+        {
+            DoOperation(10, 2, Multiply);
+            DoOperation(10, 2, Divide);
+            Console.Read();
+        }
+
+        static void DoOperation(int x, int y, Arithmetic Adel)  //delegate type as a parameter
+        {
+            int z = Adel(x, y);
+            Console.WriteLine(z);
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
--- CSharp/Day8_Dotnet/Day8_Dotnet/Multicast.cs
+++ CSharp/Day8_Dotnet/Day8_Dotnet/Multicast.cs
@@ -7,21 +7,41 @@ using System.Threading.Tasks;
 namespace Day8_Dotnet
 {
     public delegate void MultiDel();
+    public delegate int MultiDelRet(int x);
     class Multicast
     {
         static void Main()
         {
             Multicast mc = new Multicast();
 
-            MultiDel md = new MultiDel(mc.Method1);
-            md += mc.Method2;
-            md += mc.Method3;
+            //option 1 with many delegate objects cumulating into a single object
+            MultiDel md, md1, md2, md3;
+            md1 = new MultiDel(mc.Method1);
+            md2 = new MultiDel(mc.Method2);
+            md3 = new MultiDel(mc.Method3);
 
+            md = md2 + md1 + md3;
             md();
-            //remove one method from the delgate
-            Console.WriteLine("--------------");
-            md -= mc.Method2;
+
+            md -= md3;
             md();
+
+            //option 2 - Multicasting with just one delegate object
+            //MultiDel md = new MultiDel(mc.Method1);
+            //md += mc.Method2;
+            //md += mc.Method3;
+
+            //md();
+            ////remove one method from the delgate
+            //Console.WriteLine("--------------");
+            //md -= mc.Method2;
+            //md();
+            Console.WriteLine("---Multicast with return type------");
+            MultiDelRet mdr = new MultiDelRet(MulticastWithReturn.Square);
+            mdr += MulticastWithReturn.Triple;
+
+            int result = mdr(5);
+            Console.WriteLine(result);
             Console.Read();
         }
 
@@ -40,4 +60,25 @@ namespace Day8_Dotnet
             Console.WriteLine("Method 3...");
         }
     }
+
+    //example 2 for multicast with valid return types
+
+    class MulticastWithReturn
+    {
+        static int p;
+
+        public static int Square(int x)
+        {
+            p = x * x;
+            return p;
+        }
+
+        public static int Triple(int y)
+        {
+            p += y * y * y;
+            return p;
+        }
+
+    }
+
 }
