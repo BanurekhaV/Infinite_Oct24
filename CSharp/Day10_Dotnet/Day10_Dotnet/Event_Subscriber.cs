c2f739b02b50ba54d585d1f000db8142678a984d
BanurekhaV <rekhasairam@hotmail.com>
2024-11-11 16:05:41 +0530

    csharp day9

---
+++ CSharp/Day10_Dotnet/Day10_Dotnet/Event_Subscriber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Dotnet
+{
+    public delegate string DelegateForEvent(string str);
+
+    class EventsEg    // publisher class
+    {
+        //declare an event
+        event DelegateForEvent ourEvent; //declaring an event associated with a  particular delegate
+       event DelegateForEvent secondEvent; //multiple events associated with the same delegate
+
+        //constructor
+
+        public EventsEg()
+        {
+            this.ourEvent += new DelegateForEvent(this.WelcometheUser);
+            this.secondEvent += new DelegateForEvent(this.SomeOtherFunction);
+            this.secondEvent += new DelegateForEvent(Event_Subscriber.HandlerFunction);
+        }
+
+        public string WelcometheUser(string uname)
+        {
+            return "Welcome User : " + uname;
+        }
+
+        public string SomeOtherFunction(string s)
+        {
+            Console.WriteLine(s + " " + "Hope you are all enjoying the session ?.");
+            return s + " " + "Hope you are all enjoying the session ?.";
+        }
+
+        static void Main(string[] args)
+        {
+            EventsEg eventobj = new EventsEg();
+            //1. calling the function with the help of the class object
+
+            string res = eventobj.WelcometheUser("Banurekha");
+            Console.WriteLine(res);
+
+            //2. calling the class function thru delegate object
+            DelegateForEvent dfe = eventobj.WelcometheUser;
+
+            res = dfe("Prakruthi");
+            Console.WriteLine(res);
+
+            //3. caaling the function thru event
+
+            res = eventobj.ourEvent("Infinite Associates..");  // raising an evenet
+            Console.WriteLine(res);
+
+            string secondresult = eventobj.secondEvent("Infinite Accociates.");
+            Console.WriteLine(secondresult);
+            Console.Read();
+        }
+    }
+
+    class Event_Subscriber
+    {
+        public static string HandlerFunction(string s)
+        {
+            return " This was the data in the Argument " + s;
+        }
+    }
+}
---
+++ CSharp/Day10_Dotnet/Day10_Dotnet/EventsEg2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Dotnet
+{
+    class EventsEg2
+    {
+        int data = 5;
+        //decl delegate
+        public delegate void NumberManipulator(int x);
+
+        //decl event
+        event NumberManipulator ChangeNumberEvent;
+
+        public EventsEg2(int n)
+        {
+            this.ChangeNumberEvent += new NumberManipulator(this.onChangeNum); // registering an event with the
+                                                                              //delegate and hence function
+
+            setValue(n);
+        }
+
+        public void onChangeNum(int x)  //event handler
+        {
+           // data = x;
+            Console.WriteLine($"Event Raised and the value from {data} changed to {x}");
+        }
+
+        public void setValue(int n)
+        {
+            if(data != n)
+            {
+                ChangeNumberEvent(n);  //this is the place of raising an event
+            }
+            else
+            {
+                Console.WriteLine("No changes in the value... and hence no event raised and notified");
+            }
+        
+        
+        }
+    }
+
+    class TestEvent
+    {
+        static void Main()
+        {
+            EventsEg2 eobj = new EventsEg2(5);  // noevent
+            eobj.setValue(10);  //event is raised
+            eobj.setValue(20); // event is raised
+            eobj.setValue(5); // no event
+            Console.Read();
+        }
+    }
+}
