0ff3ca15db5d950fcb994765076d37864da99cbe
BanurekhaV <rekhasairam@hotmail.com>
2024-11-11 11:36:05 +0530

    csharp day9

---
+++ CSharp/Day9_Dotnet/Day9_Dotnet/AnonymousDelegatesEg1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_Dotnet
+{
+    delegate void Print(int v);
+    class AnonymousDelegatesEg1
+    {
+        static void Main()
+        {
+            int i = 100;
+            Print p = delegate (int x)  //delegate object making a call to an anonymous function
+            {
+                x += i;
+                Console.WriteLine("We are inside Anonylous Method {0}", x);
+            };
+
+            p(10);
+
+            Console.WriteLine("Try again....");
+            p(20);
+
+            Console.WriteLine("-----Example 2 inbuilt Predicate Delegate ------");
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] evennos = Array.FindAll(numbers, delegate (int num)
+             {
+                 return num % 2 == 0;
+             });
+
+            Console.WriteLine("----Even Nos collection------");
+            
+            foreach(int item in evennos)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("User Defined Predicate Delegate");
+
+            Predicate<string> chkupper = delegate (string s)
+             {
+                 return s.Equals(s.ToUpper());
+             };
+
+            bool res = chkupper("hello world");
+
+            Console.WriteLine(res);
+            Console.WriteLine(chkupper("HELLO WORLD"));
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day9_Dotnet/Day9_Dotnet/Co_ContravarianceEg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_Dotnet
+{
+
+    public class Person { }
+    public class Employees : Person { }
+
+    public class Manager : Employees { }
+
+    delegate Person persondelegate(Employees emp);  //argument is more derived type
+    class Co_ContravarianceEg
+    {
+        static void Main(string[] args)
+        {
+            //covariance 
+
+            var personobj = new Person();
+            var empobj = new Employees();
+            var mgrobj = new Manager();
+
+            personobj = empobj; //or personobj = new Employees();
+            personobj = mgrobj; // or personobj = new Manager();
+
+            empobj = mgrobj;
+
+            empobj = (Employees)personobj;
+
+            //contravariance
+            persondelegate pd = WishPerson;          
+
+        }
+
+        static Person WishPerson(Person p)  //parameter passed is less derived type
+        {
+            Console.WriteLine("Hello and Welcome Mr/Ms " + p);
+            return p;
+        }
+    }
+}
