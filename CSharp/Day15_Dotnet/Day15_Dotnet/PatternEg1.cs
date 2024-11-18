fe69e1324d181df0d82efd78bc20f5c028a5aad8
BanurekhaV <rekhasairam@hotmail.com>
2024-11-18 13:36:37 +0530

    day 15 csharp

---
+++ CSharp/Day15_Dotnet/Day15_Dotnet/PatternEg1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_Dotnet
+{
+    class Student
+    {
+        public string FName { get; private set; }
+        public string LName { get; private set; }
+
+        public Student(string fn, string ln)
+        {
+            this.FName = fn;
+            this.LName = ln;
+        }
+    }
+    class PatternEg1
+    {
+        static void Main()
+        {
+            Student stud = new Student("Amitabh", "Bacchan");
+            Student st = null;
+            int? a = 25;
+           // switch(stud)
+          // switch(st)
+            switch(a)
+            {
+                case null: Console.WriteLine("Constant Pattern");break;
+               // case Student s when s.FName.StartsWith("A"): Console.WriteLine(s.FName);break;
+                case var x: Console.WriteLine(x?.GetType().Name);break;
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day15_Dotnet/Day15_Dotnet/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_Dotnet
+{
+    class Program
+    {
+        int[] array = new int[5];
+        #region
+        static void Main(string[] args)
+        {
+            //var a = Divide(10, 0);
+            //Console.WriteLine(a);
+            Program p = new Program();
+            try
+            {
+                p.Show(p.array);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + e.StackTrace.ToString());
+                Console.WriteLine("The Name of the method where exception occured is " + nameof(p.Show));
+            }
+            Console.Read();
+        }
+        #endregion
+        #region
+        int Show(int[] a)
+        {
+            a[5] = 10;
+            return a[5];
+        }
+        #endregion
+
+        /// <summary>
+        /// Divide helps in returning exceptions if it fails
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static double Divide(int x, int y)
+        {
+            try
+            {
+                //throws allows us to throw an exception directly from an expression
+                return y != 0 ? x % y : throw new DivideByZeroException();
+            }
+
+            catch (DivideByZeroException) { Console.WriteLine("Error"); }
+            return -1;
+        }
+    }
+}
