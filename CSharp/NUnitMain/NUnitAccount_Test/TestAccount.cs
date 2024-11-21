4530e06b52aa93436bdc88092eb147ac845b7b4d
BanurekhaV <rekhasairam@hotmail.com>
2024-11-21 18:25:52 +0530

    DI + Testing

--- CSharp/NUnitMain/NUnitAccount_Test/TestAccount.cs
+++ CSharp/NUnitMain/NUnitAccount_Test/TestAccount.cs
@@ -13,7 +13,14 @@ namespace NUnitAccount_Test
     public class TestAccount
     {
         //arrange 
-        Account acc = new Account("12345");
+        Account acc;
+
+        [SetUp]
+        
+        public void ArrangeData()
+        {
+            acc = new Account("12345");
+        }
 
         [Test]
         public void TestDeposit()
@@ -32,6 +39,51 @@ namespace NUnitAccount_Test
         public void TestWithdrawalException()
         {
             ClassicAssert.Throws<Exception>(TestWithdraw);
+        }
+
+        [Test]
+        public void Testing_Login()
+        {
+            //arrange
+
+            Employee emp = new Employee();
+
+            //act
+            string s1 = emp.Login("Banurekha", "AAA");
+            string s2 = emp.Login("", "");
+            string s3 = emp.Login("Admin", "Admin@123");
+
+            //assert
+            ClassicAssert.AreEqual("User Id or Password cannot be empty", s2);
+            ClassicAssert.AreEqual("Incorrect UserId or Password", s1);
+            ClassicAssert.AreEqual("Welcome Admin", s3);
+        }
+
+        [Test]
+        [TestCase(15,35,60)]
+        [TestCase(10,45,55)]
+        [TestCase(20,50,70)]
+
+        public void TestingAdd2Nos_withTestcases(int n1, int n2, int expected)
+        {
+            Employee e = new Employee(); //arrange
+            int res = e.Add2Nos(n1, n2);
+            ClassicAssert.AreEqual(expected, res);
+        }
+
+        [Test]
+        [Ignore("waitforsometime")]
+
+        public void TestemployeeDetails()
+        {
+            Employee e = new Employee();
+            List<Employee> elist = e.Empdata();
+
+            foreach(var x in elist)
+            {
+                ClassicAssert.IsNotNull(x.ID);
+                ClassicAssert.IsNotNull(x.Name);
+            }
         }
     }
 }
--- CSharp/NUnitMain/NUnitMain/Account.cs
+++ CSharp/NUnitMain/NUnitMain/Account.cs
@@ -36,4 +36,42 @@ namespace NUnitMain
                 throw new Exception("Not Enough Funds");
         }
     }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Gender { get; set; }
+
+        public List<Employee>Empdata()
+        {
+            List<Employee> emplist = new List<Employee>()
+            {
+                new Employee{ID=100, Name="Aarthi", Gender="Female", Salary=45000},
+                new Employee{ID=101, Name="BhanuPrakash", Gender="Male", Salary=45000},
+                new Employee{ID=102, Name=null, Gender="Male", Salary=45000},
+            };
+            return emplist;
+        }
+
+        public string Login(string userid, string password)
+        {
+            if (string.IsNullOrEmpty(userid) || string.IsNullOrEmpty(password))
+            {
+                return "User Id or Password cannot be empty";
+            }
+            else if (userid == "Admin" && password == "Admin@123")
+            {
+                return "Welcome Admin";
+            }
+            else
+                return "Incorrect UserId or Password";
+        }
+
+        public int Add2Nos(int x, int y)
+        {
+            return x + y;
+        }
+    }
 }
