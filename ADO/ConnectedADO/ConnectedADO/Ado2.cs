da8820231c39607ea3fb854b3e0eddaa30b53e50
BanurekhaV <rekhasairam@hotmail.com>
2024-12-05 16:18:00 +0530

    ado 1

--- ADO/ConnectedADO/ConnectedADO/Ado2.cs
+++ ADO/ConnectedADO/ConnectedADO/Ado2.cs
@@ -14,18 +14,30 @@ namespace ConnectedADO
         public string RegionDescription { get; set; }
 
         DataAccess da = new DataAccess();
-        internal void InsertRegion()
+        internal int InsertRegion()
         {
             Console.WriteLine("Enter The Region ID :");
             RegionID = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the Region Description :");
             RegionDescription = Console.ReadLine();
-            da.InsertRegion(RegionID, RegionDescription);
+            int res=da.InsertRegion(RegionID, RegionDescription);
+            return res;
         }
 
-        internal void SelectRegions()
+        internal SqlDataReader SelectRegions()
         {
-            da.SelectData();
+            SqlDataReader dr = da.SelectData();
+            return dr;
+        }
+
+        public void CallProc()
+        {
+            da.CallProc();
+        }
+
+        public void GetRegionCount()
+        {
+            da.GetRegionCount();
         }
     }
 
@@ -43,43 +55,75 @@ namespace ConnectedADO
             return conn;
         }
 
-        public void SelectData()
+        public SqlDataReader SelectData()
         {
-            try
-            {
+            //try
+            //{
                 conn = getConnection();
                 cmd = new SqlCommand("Select * from Region", conn);
                 SqlDataReader dr = cmd.ExecuteReader();
+           
+                return dr;
                 
-                while(dr.Read())
-                {
-                    Console.WriteLine(dr["RegionID"] + " " + dr["RegionDescription"]);
-                }
-            }
-            catch(SqlException se)
-            {
-                Console.WriteLine(se.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
+                //while(dr.Read())
+                //{
+                //    Console.WriteLine(dr["RegionID"] + " " + dr["RegionDescription"]);
+                //}
+            //}
+            //catch(SqlException se)
+            //{
+            //    Console.WriteLine(se.Message);
+            //}
+            //finally   
+            //{
+
+            //    conn.Close();                
+            //}
         }
 
-        public void InsertRegion(int rid, string rdesc)
+        public int InsertRegion(int rid, string rdesc)
         {
-            try
-            {
+            int res;
+            //try
+            //{
                 conn = getConnection();
                 cmd = new SqlCommand("insert into region values(@rgid,@rgdesc)", conn);
                 cmd.Parameters.AddWithValue("@rgid", rid);
                 cmd.Parameters.AddWithValue("@rgdesc", rdesc);
-                cmd.ExecuteNonQuery();                
-            }
-            catch(SqlException se)
+                res=cmd.ExecuteNonQuery();
+                return res;
+            //}
+            //catch(SqlException se)
+            //{
+            //    Console.WriteLine(se.Message);
+            //}
+            
+        }
+
+        public void CallProc()
+        {
+            conn = getConnection();
+            cmd = new SqlCommand("Ten Most Expensive Products", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            //execute scalar will ignore all values except for the first value(0,0)
+            //object o = cmd.ExecuteScalar().ToString();
+            //Console.WriteLine(o);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
             {
-                Console.WriteLine(se.Message);
+                Console.WriteLine(dr[0] + "  " + dr[1]);
             }
+        }
+
+        //executing function that return scalar data
+        public void GetRegionCount()
+        {
+            conn = getConnection();
+            cmd = new SqlCommand("select count(regionid) from region", conn);
+            int count=Convert.ToInt32(cmd.ExecuteScalar());
+            Console.WriteLine("No of Regions Available is {0}",count);
         }
     }
     class Ado2
@@ -87,12 +131,23 @@ namespace ConnectedADO
         public static void Main()
         {
             Region region = new Region();
-            region.SelectRegions();
             Console.WriteLine();
-            region.InsertRegion();
+            //int res=region.InsertRegion();
+            //if(res>0)
+            //    Console.WriteLine("Record Successfully");
+            //else
+            //    Console.WriteLine("Failed to Insert");
             Console.WriteLine();
-            region.SelectRegions();
-            Console.Read();
+            SqlDataReader dr=region.SelectRegions();
+            while(dr.Read())
+            {                
+                    Console.WriteLine($"The Region Id is {dr["RegionID"] } and the Description is {dr["RegionDescription"]}");                
+            }
+            Console.WriteLine("--------Calling Procedure with no Input/OutPut-------");
+            region.CallProc();
+            Console.WriteLine("---------Region Count----------");
+            region.GetRegionCount();
+             Console.Read();
         }
     }
 }
