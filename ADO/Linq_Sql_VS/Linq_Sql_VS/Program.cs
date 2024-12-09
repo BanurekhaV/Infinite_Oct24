88f1539a7762e8080f135a7036f6bf4142fe254b
BanurekhaV <rekhasairam@hotmail.com>
2024-12-09 18:36:32 +0530

    Linq to sql

---
+++ ADO/Linq_Sql_VS/Linq_Sql_VS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Sql_VS
+{
+    class Program
+    {
+        static MydbmlDataContext db = new MydbmlDataContext();
+        static void Main(string[] args)
+        {
+            var emp = db.tblEmployees.ToList();
+
+            foreach(var e in emp)
+            {
+                Console.WriteLine($"Empid {e.EmpId}, EmployeeName {e.EmpName} and salary is {e.Salary}");
+            }
+            Console.Read();
+        }
+    }
+}
