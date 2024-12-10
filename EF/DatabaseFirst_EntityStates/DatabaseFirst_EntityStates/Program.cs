864659ee1d0239ad132604eff6dda4cfe67c6f67
BanurekhaV <rekhasairam@hotmail.com>
2024-12-10 13:31:33 +0530

    Ef database First

---
+++ EF/DatabaseFirst_EntityStates/DatabaseFirst_EntityStates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst_EntityStates
+{
+    class Program
+    {
+        //let us create an instance of the database Context class
+
+        static InfiniteDBEntities db = new InfiniteDBEntities();
+        static void Main(string[] args)
+        {
+            var emp = db.tblEmployees.ToList();
+
+            foreach(var item in emp)
+            {
+                Console.WriteLine($"Employee : {item.EmpName} Earns a Salary of : {item.Salary}, and works for Department: {item.DeptId}");
+            }
+            Console.Read();
+        }
+    }
+}
