72aaaeab32bf007d5efcf04eec28e77cc315aede
BanurekhaV <rekhasairam@hotmail.com>
2024-12-20 18:02:05 +0530

    Assignment 1

---
+++ ASP/Caching_Prj/Caching_Prj/RedirectVsTransfer.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RedirectVsTransfer.aspx.cs" Inherits="Caching_Prj.RedirectVsTransfer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name  :  &nbsp;&nbsp;&nbsp;<asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Email  : &nbsp;&nbsp;&nbsp; <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="BtnGetData" runat="server" Text="GetPage" OnClick="BtnGetData_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/Caching_Prj/Caching_Prj/RedirectVsTransfer.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Caching_Prj
+{
+    public partial class RedirectVsTransfer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnGetData_Click(object sender, EventArgs e)
+        {
+            Context.Items.Add("Name", txtname.Text);
+            Context.Items.Add("Email", txtemail.Text);
+
+            //1. Redirect
+             Response.Redirect("OtherPage.aspx"); //this resource is in the same web server
+
+            // Response.Redirect("https://www.amazon.in"); //this is from other webserver
+
+            //2. Transfer
+          // Server.Transfer("OtherPage.aspx");
+
+           // Server.Transfer("https://www.amazon.in"); 
+        }
+    }
+}
