f4c6e54ad8bfcaae7dc71f816760049dc80a8c94
BanurekhaV <rekhasairam@hotmail.com>
2024-12-26 18:33:21 +0530

    mvc basics

---
+++ MVC/MVC_Basics/MVC_Basics/Controllers/DataController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Basics.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        //1. passing an object to the view that will be used as a model
+        public ActionResult Index()
+        {
+            List<string> flowers = new List<string>()
+            { "Roses", "Lillies", "Jasmines", "Marigolds"};
+            return View(flowers);
+        }
+
+        //2. Passing data thru viewbag and viewdata
+        public ActionResult officeRules()
+        {
+            List<string> rules = new List<string>()
+            {
+                "Be on time", "Carry ID card", "Avoid TShirts","Complete Work as per deadlines"
+            };
+            //1. transfer the data using viewbag
+            // ViewBag.offrules = rules;
+
+            //2. trabsfer data using viewdata
+            ViewData["followrules"] = rules;
+            return View();
+        }
+    }
+}
