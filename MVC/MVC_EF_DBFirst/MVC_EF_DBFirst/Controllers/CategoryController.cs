1cf89ac9424cac2fb3234d8cd6de49d9a99743cf
BanurekhaV <rekhasairam@hotmail.com>
2024-12-27 13:28:09 +0530

    EFDB First

---
+++ MVC/MVC_EF_DBFirst/MVC_EF_DBFirst/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_EF_DBFirst.Models;
+
+namespace MVC_EF_DBFirst.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Category
+        //1. The below action method uses scaffolded view template
+        public ActionResult Index()
+        {
+            List<Category> catlist = db.Categories.ToList();
+            return View(catlist);
+        }
+
+        //2. The below action method does not use scaffolded view
+        public ActionResult GetCategoryDetails()
+        {
+            List<Category> catlist = db.Categories.ToList();
+            return View(catlist);
+        }
+
+        //3. Add a new Category
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category c)
+        {
+            db.Categories.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
