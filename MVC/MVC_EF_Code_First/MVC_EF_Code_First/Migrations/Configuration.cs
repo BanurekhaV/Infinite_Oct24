c12195207157a38b3467b14e2380356a05a73237
BanurekhaV <rekhasairam@hotmail.com>
2024-12-30 13:35:14 +0530

    code first

---
+++ MVC/MVC_EF_Code_First/MVC_EF_Code_First/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace MVC_EF_Code_First.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVC_EF_Code_First.Models.ProductsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MVC_EF_Code_First.Models.ProductsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
