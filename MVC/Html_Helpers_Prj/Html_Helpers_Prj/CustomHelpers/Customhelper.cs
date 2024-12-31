012041347f625593113020039665cefced64dae0
BanurekhaV <rekhasairam@hotmail.com>
2024-12-31 11:42:44 +0530

    Html Helpers

---
+++ MVC/Html_Helpers_Prj/Html_Helpers_Prj/CustomHelpers/Customhelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Html_Helpers_Prj.CustomHelpers
+{
+    public class Customhelper
+    {
+        public static IHtmlString LabelWithMark(string content)
+        {
+            string htmlstr = String.Format("<label><b><mark><i><del>{0}</del></i></mark></b></label>", content);
+            return new HtmlString(htmlstr);
+        }
+    }
+}
