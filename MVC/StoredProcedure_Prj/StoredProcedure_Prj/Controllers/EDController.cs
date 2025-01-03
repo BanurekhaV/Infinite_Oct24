cdc5fadaf4b4f4b99e8fc029a6f25a79c646aa8b
BanurekhaV <rekhasairam@hotmail.com>
2025-01-03 10:47:45 +0530

    storedproc for ef

---
+++ MVC/StoredProcedure_Prj/StoredProcedure_Prj/Controllers/EDController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoredProcedure_Prj.Models;
+
+namespace StoredProcedure_Prj.Controllers
+{
+    public class EDController : Controller
+    {
+        EDContext db = new EDContext();
+        // GET: ED
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        //create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            Employee e = db.Employees.Find(Id);
+
+            return View(e);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            Employee emp = db.Employees.Find(e.Id);
+            return View();
+        }
+    }
+}
---
+++ MVC/StoredProcedure_Prj/StoredProcedure_Prj/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoredProcedure_Prj.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DId { get; set; }
+        public string DName { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
